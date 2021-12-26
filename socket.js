9cf0f49c0a18030c85b1b81711387a0ecf36d5c2
MaksymStarodubtsev1 <88541521+MaksymStarodubtsev1@users.noreply.github.com>
2021-12-26 23:50:15 +0200

    Add files via upload

---
+++ socket.js
@@ -0,0 +1,59 @@
+const io = require('socket.io')(808);
+
+const axios = require('axios')
+
+
+function post(request) {
+  axios.post('https://vue-http-demo-763e4-default-rtdb.europe-west1.firebasedatabase.app/olenamaksym.json',{
+  body: JSON.stringify(request)
+}).then((res) => {
+  console.log(res.statusCode)
+})
+}
+
+// }) {
+//   fetch('https://vue-http-demo-763e4-default-rtdb.europe-west1.firebasedatabase.app/olenamaksym.json', {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify(request)
+//   })
+//   .then(() => {reloadMessages()})
+// }
+
+// function get() {
+//   return fetch('https://vue-http-demo-763e4-default-rtdb.europe-west1.firebasedatabase.app/olenamaksym.json')
+//   .then((response) => response.json())
+//   .then((d) => d)
+// }
+
+function get() {
+  return axios.get('https://vue-http-demo-763e4-default-rtdb.europe-west1.firebasedatabase.app/olenamaksym.json')
+  .then((response) => console.log(response.data))
+}
+
+io.on('connection', (socket) => {
+  console.log('socket.id',socket.id)
+
+
+  socket.on('firstEvent', (e) => {
+    console.log('firstEvent',e)
+  })
+  socket.emit('secondEvent', 'hello from Server')
+
+  socket.on('sendMessage', (message) => {
+    post(JSON.parse(message))
+    console.log('message', message)
+    async function send(){
+      const request = await get()
+      socket.broadcast.emit('updatedMessages', request)
+    }
+    send()
+  })
+
+  socket.on('disconnect', (socket) => {
+    console.log('disconnect', socket);
+  })
+})
+

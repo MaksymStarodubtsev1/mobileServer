87a72834164fe84653c8753bfff71d5210693e65
MaksymStarodubtsev1 <88541521+MaksymStarodubtsev1@users.noreply.github.com>
2021-12-28 13:38:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,80 +1,65 @@
-const io = require('socket.io') (1919);
+const io = require('socket.io')(3000);
 
-const fs = require('fs');
-const axios = require('axios');
-let rageData = [];
-let altData = [];
+const express = require('express');
+const app = express();
+const http = require('https');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+//const io = new Server(server);
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
-setInterval(() => {
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true}));
 
-    rageData = [];
-    altData = [];
-
-    console.clear();
-    axios.get(`https://cdn.rage.mp/master/`).then(function (res) {
-        let data = res.data;
-        for (var i = 0; i < rage.length; i++){
-            let info = data[rage[i].ip + ":" + rage[i].port];
-            if (info != undefined) {
-                let servData = {
-                    "name": info.name,
-                    "gamemode": info.gamemode,
-                    "url": info.url,
-                    "lang": info.lang,
-                    "players": info.players,
-                    "peak": info.peak,
-                    "maxplayers": info.maxplayers,
-                    "ip": rage[i].ip,
-                    "port": rage[i].port
-                };
-                rageData.push(servData);
-            } else {
-                console.log(`[RAGE]  + rage[i].host + ':' + rage[i].port +  выключен или недоступен.`);
-            }
-        }
-        console.log(`RAGE:MP JSON оновлён. Результат: ` + rageData.length);
-    });
-
-    axios.get(`https://api.altv.mp/servers/list`).then(function (res) {
-        let data = res.data;
-        for (var i = 0; i < alt.length; i++) {
-            let info = data.find(server => server.host == alt[i].host && server.port == alt[i].port);
-            if (info != undefined) {
-                let servData = {
-                    "name": info.name,
-                    "gamemode": info.gameMode,
-                    "url": info.website,
-                    "lang": info.language,
-                    "players": info.players,
-                    "peak": null,
-                    "maxplayers": info.maxPlayers,
-                    "ip": info.host,
-                    "port": info.port
-                };
-                altData.push(servData);
-            } else {
-                console.log(`[ALT] ` + alt[i].host + ':' + alt[i].port + ' выключен или недоступен.');
-            }
-        }
-        console.log(`ALT:V JSON оновлён. Результат: ` + altData.length);;
-    });
-
-    let date = new Date();
-    let h = date.getHours();
-    let m = date.getMinutes();
-
-    console.log(`[TIME] ${h}:${m}`);
-}, 30000);
-
-io.on('connection', socket => {
-    console.log('Новое подключение установлено с ' + socket.id);
-    socket.on('requestServerData', (ip, port, platform) => {
-        if (platform == 'rage') {
-            let serverData = rageData.find(server => server.ip == ip && server.port == port);
-            socket.emit('receiveServerData', serverData);
-        } else {
-            let serverData = altData.find(server => server.ip == ip && server.port == port);
-            socket.emit('receiveServerData', serverData);
-        }
-    })
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
 });
+
+app.get('/test', (req, res) => {
+  res.send('Hello, I"m working!');
+});
+
+const axios = require('axios')
+
+
+function post(request) {
+  axios.post('https://vue-http-demo-763e4-default-rtdb.europe-west1.firebasedatabase.app/olenamaksym.json',{
+  body: JSON.stringify(request)
+}).then((res) => {
+  console.log(res.statusCode)
+})
+}
+
+
+function get() {
+  return axios.get('https://vue-http-demo-763e4-default-rtdb.europe-west1.firebasedatabase.app/olenamaksym.json')
+  .then((response) => console.log(response.data))
+}
+
+io.on('connection', (socket) => {
+  console.log('socket.id',socket.id)
+
+  socket.on('firstEvent', (e) => {
+    console.log('firstEvent',e)
+  })
+  socket.emit('secondEvent', 'hello from Server')
+
+  socket.on('sendMessage', (message) => {
+    post(JSON.parse(message))
+    console.log('message', message)
+    async function send(){
+      const request = await get()
+      socket.broadcast.emit('updatedMessages', request)
+    }
+    send()
+  })
+
+  socket.on('disconnect', (socket) => {
+    console.log('disconnect', socket);
+  })
+})
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('listening on *:3000');

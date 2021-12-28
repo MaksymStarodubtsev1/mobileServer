d3ff6da51e82190fbefc09949fe2e30360ffdd83
MaksymStarodubtsev1 <88541521+MaksymStarodubtsev1@users.noreply.github.com>
2021-12-28 22:15:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,6 @@
-const io = require('socket.io')(3000);
-
-const express = require('express');
-const app = express();
-const http = require('https');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-//const io = new Server(server);
-const cors = require('cors');
-const bodyParser = require('body-parser');
-
-app.use(cors());
-app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true}));
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
-
-app.get('/test', (req, res) => {
-  res.send('Hello, I"m working!');
-});
+const io = require('socket.io') (1919);
 
 const axios = require('axios')
-
 
 function post(request) {
   axios.post('https://vue-http-demo-763e4-default-rtdb.europe-west1.firebasedatabase.app/olenamaksym.json',{
@@ -31,7 +9,6 @@ function post(request) {
   console.log(res.statusCode)
 })
 }
-
 
 function get() {
   return axios.get('https://vue-http-demo-763e4-default-rtdb.europe-west1.firebasedatabase.app/olenamaksym.json')
@@ -60,6 +37,3 @@ io.on('connection', (socket) => {
     console.log('disconnect', socket);
   })
 })
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log('listening on *:3000');

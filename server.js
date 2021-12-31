10574dfc2c725930740f71b44d5a14b9297abcec
MaksymStarodubtsev1 <88541521+MaksymStarodubtsev1@users.noreply.github.com>
2021-12-31 20:24:02 +0200

    Update server.js

--- server.js
+++ server.js
@@ -17,4 +17,42 @@ io.on('connection', (socket) => {
   socket.on('disconnect', () => console.log('Client disconnected'));
 });
 
-setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+// setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+
+const axios = require('axios')
+
+function post(request) {
+  axios.post('https://vue-http-demo-763e4-default-rtdb.europe-west1.firebasedatabase.app/olenamaksym.json',{
+  body: JSON.stringify(request)
+}).then((res) => {
+  console.log(res.statusCode)
+})
+}
+
+function get() {
+  return axios.get('https://vue-http-demo-763e4-default-rtdb.europe-west1.firebasedatabase.app/olenamaksym.json')
+  .then((response) => console.log(response.data))
+}
+
+io.on('connection', (socket) => {
+  console.log('socket.id',socket.id)
+
+  socket.on('firstEvent', (e) => {
+    console.log('firstEvent',e)
+  })
+  socket.emit('secondEvent', 'hello from Server')
+
+  socket.on('sendMessage', (message) => {
+    post(JSON.parse(message))
+    console.log('message', message)
+    async function send(){
+      const request = await get()
+      socket.broadcast.emit('updatedMessages', request)
+    }
+    send()
+  })
+
+  socket.on('disconnect', (socket) => {
+    console.log('disconnect', socket);
+  })
+})
